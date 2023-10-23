ad40b2b3cb8114394b211990deab01b50f14e5bf
Mahad1000 <2114931@brunel.ac.uk>
2023-10-23 18:11:12 +0100

    update 2
    if theres complications go back to number 1

--- Platformer/Assets/PlayerMovement.cs
+++ Platformer/Assets/PlayerMovement.cs
@@ -3,19 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
+{ 
+    private Animator anim;
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     void Start()
     {
-           
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
+        horizontal = Input.GetAxisRaw("Horizontal");
+        
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+        PlayAnimation();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void PlayAnimation()
+    {
+        bool isRunning = Mathf.Abs(horizontal) > 0f;
+        anim.SetBool("Running", isRunning);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
         }
     }
 }
